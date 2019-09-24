0cc4437c92276c5cc2fd91a6c74df7dd1809e23b
James Jessen <JSJessen@gmail.com>
2019-09-23 23:51:52 -0700

    Removed specialized code from classification func

--- 4hw/PerceptronClassification.py
+++ 4hw/PerceptronClassification.py
@@ -7,11 +7,7 @@
 import pandas as pd
 import numpy as np
 
-def perceptronClassification(dataPath, inputColumns, classColumn,
-    normalizeInputs = False,
-    emptyCellHandling = 'drop', 
-    outputPrecision = 2):
-
+def perceptronClassification(dataPath, inputColumns, classColumn):
     # print("Input Columns: {}".format(inputColumns))
     # print("Class Column: {}".format(classColumn))
 
@@ -26,26 +22,6 @@ def perceptronClassification(dataPath, inputColumns, classColumn,
     numInputs = len(inputColumns)
     numRows = len(data.index)
 
-    if emptyCellHandling.lower() == 'zero':
-        print("Empty cells have been set to zero.")
-        data = data.fillna(0)
-    elif emptyCellHandling.lower() == 'drop':
-        numRowsBefore = numRows
-        data = data.dropna()
-        numRows = len(data.index)
-        numRowsDropped = numRowsBefore - numRows
-        if numRowsDropped > 0:
-            print("{} rows with empty cells have been dropped.".format(numRowsDropped))
-    else:
-        print("Warning: Empty cells not handled.")
-
-    # Normalize Data
-    if normalizeInputs:
-        # TODO: Prevent class column from being affected
-        data = (data - data.min()) / (data.max() - data.min())
-    else:
-        print("Warning: Not normalizing inputs")
-
     V = np.ones((numRows, 1 + numInputs)) # First column ones for bias node
     V[:,1:] = data.iloc[:,inputColumns].values
     
@@ -54,18 +30,18 @@ def perceptronClassification(dataPath, inputColumns, classColumn,
     # Solve the normal equation
     w = np.linalg.solve(V.T.dot(V), V.T.dot(classifications))
 
-    fit = V.dot(w)
-    yAvg = np.mean(classifications) # AKA yBar
+    fits = V.dot(w)
+    yAvg = np.mean(classifications)
 
     # Sum of Squares Regression
     #   Measures variability of fit from mean response.
-    ssrDiff = fit - yAvg
+    ssrDiff = fits - yAvg
     SSR = ssrDiff.dot(ssrDiff)
 
     # Sum of Squares Error
     #   Measures variability of response from all other sources after the linear relationship 
     #   between response and attributes has been accounted for.
-    residuals = classifications - fit # Deviations predicted from actual empirical values of data
+    residuals = classifications - fits # Deviations predicted from actual empirical values of data
     SSE = residuals.dot(residuals)
 
     # Sum of Squares Total 
@@ -89,51 +65,4 @@ def perceptronClassification(dataPath, inputColumns, classColumn,
     s = np.sqrt(MSE)
     # Can be lower or higher when another attribute is added to the model.
 
-    classLabels = np.unique(classifications)
-    classLabels = np.sort(classLabels)
-
-    # TODO: Move to hw4.py
-    totalLabel = 'Total'
-    accuracyLabel = 'Accuracy'
-    totals = pd.DataFrame(0, index=[totalLabel, accuracyLabel], columns=classLabels)
-    confusionMatrix = pd.DataFrame(0, index=classLabels, columns=classLabels)
-    for index in data.index:
-        classLabel = classifications[index]
-        totals.loc[totalLabel, classLabel] += 1
-        if fit[index] < 1.5: assignedClass = 1
-        elif fit[index] > 4: assignedClass = 6
-        else: assignedClass = 2
-        confusionMatrix.loc[assignedClass, classLabel] += 1
-    
-    totalCorrectlyClassified = 0
-    superTotalClassified = 0
-    for classLabel in classLabels:
-        numCorrectlyClassified = confusionMatrix.loc[classLabel, classLabel]
-        totalCorrectlyClassified += numCorrectlyClassified
-        totalClassified = totals.loc[totalLabel, classLabel]
-        superTotalClassified += totalClassified
-        totals.loc[accuracyLabel, classLabel] = str(round((numCorrectlyClassified / totalClassified) * 100, outputPrecision)) + '%'
-
-    # Report the following:
-    #     coefficient of determination (AKA R^2)
-    print("\nR² = {}%\n".format(round(rSq * 100, outputPrecision)))
-
-    #     number of records in each class and accuracy of class assignment
-    print(totals)
-    
-    #     overall accuracy
-    print("\nOverall Accuracy: {}%".format(round(((totalCorrectlyClassified / superTotalClassified) * 100), outputPrecision)))
-
-    #     3-way confusion matrix as shown below
-    print("\nConfusion Matrix")
-    print('-'*20)
-    print(confusionMatrix)
-    print('-'*20)
-    print("Row: assigned class")
-    print("Col: actual class")
-
-# in class 1 assigned class 1	# in class 2 assigned class 1 	 # in class 6 assigned class 1 
-# in class 1 assigned class 2 	# in class 2 assigned class 2 	 # in class 6 assigned class 2 
-# in class 1 assigned class 6	# in class 2 assigned class 6 	 # in class 6 assigned class 6
- 
-    return rSq, s
+    return classifications, fits, rSq
--- 4hw/hw4.py
+++ 4hw/hw4.py
@@ -9,19 +9,60 @@
 
 # python 4hw/hw4.py > 4hw/output.txt
 
+import pandas as pd
+import numpy as np
 from PerceptronClassification import perceptronClassification as pc
 
 dataPath = 'data/glassDataWithHeader.csv'
 inputColumns = list(range(0,9))
 classColumn = 9
 precision = 2
-emptyMethods = ['zero', 'drop']
+totalLabel = 'Total'
+accuracyLabel = 'Accuracy'
 
-rSq, s = pc(dataPath, inputColumns, classColumn, 
-    normalizeInputs=False,
-    emptyCellHandling='drop', 
-    outputPrecision=precision)
+classifications, fits, rSq = pc(dataPath, inputColumns, classColumn)
 
-# print('\n')
-# print("R² = {}%".format(round(rSq * 100, precision)))
-# print("s = {}".format(round(s, precision)))
+classLabels = np.unique(classifications)
+classLabels = np.sort(classLabels)
+
+totals = pd.DataFrame(0, index=[totalLabel, accuracyLabel], columns=classLabels)
+confusionMatrix = pd.DataFrame(0, index=classLabels, columns=classLabels)
+for classLabel, fit in zip(classifications, fits):
+    totals.loc[totalLabel, classLabel] += 1
+    if fit < 1.5: assignedClass = 1
+    elif fit > 4: assignedClass = 6
+    else: assignedClass = 2
+    confusionMatrix.loc[assignedClass, classLabel] += 1
+
+totalCorrectlyClassified = 0
+totalClassified = 0
+for classLabel in classLabels:
+    numCorrectlyClassified = confusionMatrix.loc[classLabel, classLabel]
+    numClassified = totals.loc[totalLabel, classLabel]
+    accuracyPercentage = (numCorrectlyClassified / numClassified) * 100
+    totals.loc[accuracyLabel, classLabel] = str(round(accuracyPercentage, precision)) + '%'
+    totalCorrectlyClassified += numCorrectlyClassified
+    totalClassified += numClassified
+
+# Report the following:
+#     coefficient of determination, R^2
+print("R² = {}%\n".format(round(rSq * 100, precision)))
+
+#     number of records in each class and accuracy of class assignment
+print(totals)
+
+#     overall accuracy
+overallAccuracy = (totalCorrectlyClassified / totalClassified) * 100
+print("\nOverall Accuracy: {}%".format(round(overallAccuracy, precision)))
+
+#     3-way confusion matrix as shown below
+print("\nConfusion Matrix")
+print('-'*20)
+print(confusionMatrix)
+print('-'*20)
+print("Row: assigned class")
+print("Col: actual class")
+
+# in class 1 assigned class 1	# in class 2 assigned class 1 	 # in class 6 assigned class 1 
+# in class 1 assigned class 2 	# in class 2 assigned class 2 	 # in class 6 assigned class 2 
+# in class 1 assigned class 6	# in class 2 assigned class 6 	 # in class 6 assigned class 6
